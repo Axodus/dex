94490b957a22027cdf97bbc3b0a9671d79a7ba62
Mauricio Z. Filho <admin@whostler.com>
2022-03-13 10:58:25 -0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,11 +7,11 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const BSC_BLOCK_TIME = 3
+export const HECO_BLOCK_TIME = 3
 
-export const BASE_BSC_SCAN_URLS = {
-  [ChainId.MAINNET]: 'https://bscscan.com',
-  [ChainId.TESTNET]: 'https://testnet.bscscan.com',
+export const BASE_HECO_SCAN_URLS = {
+  [ChainId.MAINNET]: 'https://hecoinfo.com/',
+  [ChainId.TESTNET]: 'https://testnet.hecoinfo.com/',
 }
 
 // CAKE_PER_BLOCK details
@@ -21,17 +21,18 @@ export const BASE_BSC_SCAN_URLS = {
 // 9 CAKE per block goes to Yield farms and lottery
 // CAKE_PER_BLOCK in config/index.ts = 40 as we only change the amount sent to the burn pool which is effectively a farm.
 // CAKE/Block in src/views/Home/components/CakeDataRow.tsx = 15 (40 - Amount sent to burn pool)
-export const CAKE_PER_BLOCK = 40
-export const BLOCKS_PER_YEAR = (60 / BSC_BLOCK_TIME) * 60 * 24 * 365 // 10512000
-export const CAKE_PER_YEAR = CAKE_PER_BLOCK * BLOCKS_PER_YEAR
-export const BASE_URL = 'https://pancakeswap.finance'
+export const MAKI_PER_BLOCK = 20
+export const BLOCKS_PER_YEAR = (60 / HECO_BLOCK_TIME) * 60 * 24 * 365 // 10512000
+export const MAKI_PER_YEAR = MAKI_PER_BLOCK * BLOCKS_PER_YEAR
+export const BASE_URL = 'https://axodus.one'
 export const BASE_ADD_LIQUIDITY_URL = `${BASE_URL}/add`
-export const BASE_BSC_SCAN_URL = BASE_BSC_SCAN_URLS[ChainId.MAINNET]
+export const BASE_HECO_SCAN_URL = BASE_BSC_SCAN_URLS[ChainId.MAINNET]
 export const DEFAULT_TOKEN_DECIMAL = BIG_TEN.pow(18)
 export const DEFAULT_GAS_LIMIT = 200000
+export const DEFAULT_GAS_PRICE = 5
 export const AUCTION_BIDDERS_TO_FETCH = 500
 export const RECLAIM_AUCTIONS_TO_FETCH = 500
 export const AUCTION_WHITELISTED_BIDDERS_TO_FETCH = 500
 export const IPFS_GATEWAY = 'https://ipfs.io/ipfs'
 // In reality its 10000 because of fast refresh, a bit less here to cover for possible long request times
-export const PANCAKE_BUNNIES_UPDATE_FREQUENCY = 8000
+//export const PANCAKE_BUNNIES_UPDATE_FREQUENCY = 8000
